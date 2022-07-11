74289bb9b44a03aff32614547a9a91cf819514b9
Agung Prasetya <agungprasetya1121@gmail.com>
2022-07-12 02:05:33 +0700

    first commit nextjs

---
+++ client/pages/about.js
@@ -0,0 +1,17 @@
+import Link from "next/link";
+import Layout from "@/components/Layout";
+
+const about = () => {
+  return (
+    <Layout title="About Event Ticketing">
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Esse id
+        reprehenderit unde fugiat maiores quos.
+      </p>
+      <p>version: 1.1.0</p>
+    </Layout>
+  );
+};
+
+export default about;
---
+++ client/pages/events/add.js
@@ -0,0 +1,11 @@
+import Layout from "@/components/Layout";
+
+function addEventPage() {
+  return (
+    <Layout title="Add New Event">
+      <h1>Add New Events</h1>
+    </Layout>
+  );
+}
+
+export default add;
